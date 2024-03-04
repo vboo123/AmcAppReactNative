a77d676849e7aa34e711860af4df7e6d848d51b8
ttedon <Dhruva.madhan@gmail.com>
2024-03-03 18:16:58 -0800

    Modal Implementation (Too Many Re-Renders Error)

--- AmcApp/Components/Login.js
+++ AmcApp/Components/Login.js
@@ -1,16 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
-import StudentRegister from './StudentRegister';
+import React, {useState} from 'react';
+import { StyleSheet, Text, Modal, View, TouchableOpacity, Button} from 'react-native';
 
 export default function Login({navigation})
 {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View style={styles.container}>
-
-        <Button title='Student Registration' style={styles.button} onPress={() => navigation.navigate('StudentRegister')}></Button>
-
-        <Button title='Volunteer Registration' style={styles.button} onPress={() => navigation.navigate('VolunteerRegister')}></Button>
-
+      <Button title ='Register' style={styles.button} OnPress={() => setModalVisible(true)}></Button>
+      <Modal animationType='slide' transparent={true} visible={modalVisible}>
+        <View style={styles.container}>
+          <View style={styles.modalView}>
+            <Button title='Student Registration' style={styles.button} onPress={() => navigation.navigate('StudentRegister')}{...(setModalVisible(!modalVisible))}></Button>
+            <Button title='Volunteer Registration' style={styles.button} onPress={() => navigation.navigate('VolunteerRegister')}{...(setModalVisible(!modalVisible))}></Button>
+          </View>
+        </View>
+      </Modal>
       <StatusBar style="auto" />
     </View>
   );
@@ -25,4 +30,26 @@ const styles = StyleSheet.create({
       paddingLeft: 60,
       paddingRight: 60,
     },
+    modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+   },
+   button: {
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#eee',
+    marginTop: 30,
+  }
   });
