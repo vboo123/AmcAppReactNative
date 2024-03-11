b4ccf254e89c6d041d252d6ca6d1e659b11b2684
ttedon <Dhruva.madhan@gmail.com>
2024-03-10 20:53:03 -0700

    Fixed Styling (Moved all styles to css file) and fixed login screen

--- AmcApp/Components/Login.js
+++ AmcApp/Components/Login.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, Modal, View, TouchableOpacity, Button} from 'react-native';
+import { Text, Modal, View, TouchableOpacity, TextInput } from 'react-native';
 import Styles from '../Styling/Styles.css';
 
 export default function Login({navigation})
@@ -27,12 +27,27 @@ export default function Login({navigation})
 
   return (
     <View style={Styles.container}>
+      <TextInput style={Styles.textinput} placeholder="Your Email"
+        onChangeText={newText => setEmail(newText)}
+        defaultValue={email}
+        underlineColorAndroid={'transparent'} />
+
+      <TextInput style={Styles.textinput} placeholder="Your Password"
+        onChangeText={newText => setPassword(newText)}
+        defaultValue={password}
+        secureTextEntry={true}
+        underlineColorAndroid={'transparent'} />
+
+      <TouchableOpacity style={Styles.button}>
+        <Text style={Styles.btnText}>Login</Text>
+      </TouchableOpacity>
+
       <TouchableOpacity title ='Register' style={Styles.button} onPress={() => setModalVisible(!modalVisible)}>
         <Text style={Styles.btnText}>Register</Text>
       </TouchableOpacity>
       <Modal animationType='slide' transparent={true} visible={modalVisible}>
         <View style={Styles.container}>
-          <View style={styles.modalView}>
+          <View style={Styles.modalView}>
             <TouchableOpacity style={Styles.button} onPress={() => onPressRegister('StudentRegister')}>
               <Text style={Styles.btnText}>Student Registration</Text>
             </TouchableOpacity>
@@ -46,21 +61,3 @@ export default function Login({navigation})
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-    modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-   }
-  });
--- AmcApp/Components/VolunteerRegister.js
+++ AmcApp/Components/VolunteerRegister.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 import Styles from '../Styling/Styles.css';
 
 export default function VolunteerRegister()
@@ -9,25 +9,25 @@ export default function VolunteerRegister()
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
     return(
-        <View style={styles.register}>
-            <Text style={styles.header}>Volunteer Registration</Text>
+        <View style={Styles.register}>
+            <Text style={Styles.header}>Volunteer Registration</Text>
 
-            <TextInput style={styles.textinput} placeholder="Volunteer first name"
+            <TextInput style={Styles.textinput} placeholder="Volunteer first name"
             onChangeText={newText => setFirstName(newText)}
             defaultValue={firstName}
             underlineColorAndroid={'transparent'} />
 
-            <TextInput style={styles.textinput} placeholder="Volunteer last name"
+            <TextInput style={Styles.textinput} placeholder="Volunteer last name"
             onChangeText={newText => setLastName(newText)}
             defaultValue={lastName}
             underlineColorAndroid={'transparent'} />
 
-            <TextInput style={styles.textinput} placeholder="Your Email"
+            <TextInput style={Styles.textinput} placeholder="Your Email"
             onChangeText={newText => setEmail(newText)}
             defaultValue={email}
             underlineColorAndroid={'transparent'} />
 
-            <TextInput style={styles.textinput} placeholder="Your Password"
+            <TextInput style={Styles.textinput} placeholder="Your Password"
             onChangeText={newText => setPassword(newText)}
             defaultValue={password}
             secureTextEntry={true}
@@ -39,25 +39,3 @@ export default function VolunteerRegister()
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-  register: {
-    alignSelf: 'stretch',
-  },
-  header: {
-    fontSize: 24,
-    color: '#000000',
-    paddingBottom: 10,
-    marginBottom: 40,
-    borderBottomColor: '#129923',
-    borderBottomWidth: 1,
-  },
-  textinput: {
-    alignSelf: 'stretch',
-    height: 40,
-    marginBottom: 30,
-    color: '#000000',
-    borderBottomColor: '#129923',
-    borderBottomWidth: 1,
-  }
-});
